eee8d4e2b1911efd3643254a3c036042c5eeb4b9
galaxian85 <gasbomb_tw@hotmail.com>
2021-09-04 01:15:49 +0800

    refactor: change database library to better-sqlite3

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,40 +1,23 @@
-import sqlite3 from 'sqlite3';
-const db = new sqlite3.Database(':memory:');
+import Database from 'better-sqlite3';
+
+const db = new Database(':memory:');
 
 export function initDB(): void {
-  db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS urlMapping (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      url TEXT NOT NULL
-    )`);
-  });
+  db.prepare(`CREATE TABLE IF NOT EXISTS urlMapping (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    url TEXT NOT NULL)`).run();
 }
 
 // if an url not exist then save it and retrieve auto increment id
 // if already exist, just retrieve id
-export function retrieveId(url: string): Promise<number> {
-  return new Promise((resolve, reject) => {
-    db.serialize(() => {
-      const stmt = 'SELECT id FROM urlMapping WHERE url = $url';
-      db.get(stmt, {$url: url}, (err, row) => {
-        if (row) {
-          resolve(row['id']);
-          return;
-        }
+export function retrieveId(url: string): number {
+  const row = db.prepare(`SELECT id FROM urlMapping WHERE url = ?`).get(url);
+  if (row) return row.id;
 
-        db.run('INSERT INTO urlMapping(url) VALUES ($url)', {$url: url});
-        db.get('SELECT last_insert_rowid() FROM urlMapping', (err, row) => {
-          resolve(row['last_insert_rowid()']);
-        });
-      });
-    });
-  });
+  const info = db.prepare(`INSERT INTO urlMapping (url) VALUES (?)`).run(url);
+  return info.lastInsertRowid as number;
 }
 
-export function getUrlById(id: number): Promise<string> {
-  return new Promise((resolve, reject) => {
-    db.get('SELECT url FROM urlMapping WHERE id = ?', id, (err, row) => {
-      resolve(row ? row['url'] : null);
-    });
-  });
+export function getUrlById(id: number): string {
+  return db.prepare(`SELECT url FROM urlMapping WHERE id = ?`).get(id).id;
 }
